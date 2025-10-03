14a83e7c9f08f298f5b4f598fd1ce5b236256b00
Amilka Lopez <amilka27lopez@gmail.com>
2025-10-03 12:01:51 -0600

    added icons and Emergencia Urbana form

--- test-admin/src/App.tsx
+++ test-admin/src/App.tsx
@@ -1,39 +1,53 @@
 // src/App.tsx
-import { Admin, Resource, ListGuesser, ShowGuesser, CustomRoutes } from "react-admin";
-import { Route } from "react-router-dom";
+import {
+    Admin,
+    Resource,
+    ListGuesser,
+    ShowGuesser,
+    CustomRoutes,
+} from 'react-admin';
+import { Route } from 'react-router-dom';
 
-import { Layout } from "./Layout";
-import { dataProvider } from "./dataProvider";
-import { authProvider } from "./authProvider";
-import myTheme from "./theme";
+import { Layout } from './Layout';
+import { dataProvider } from './dataProvider';
+import { authProvider } from './authProvider';
+import myTheme from './theme';
 import CustomLogin from './Login';
 
-// Recursos de react-admin
+// Recursos
 import { Dashboard } from './dashboard';
 import { Listado } from './listado';
 import { Estadisticas } from './estadisticas';
-import { MedicFormCreate } from './testForm'; // Formulario de react-admin (para Resource)
-
-// Rutas personalizadas
+import { MedicFormCreate } from './testForm';
+import UrbanFormCreate from './urbanForm';
 import Selector from './selector';
-import UrbanForm from './urbanForm';
+
+// Iconos
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
+import EngineeringIcon from '@mui/icons-material/Engineering';
 
 export const App = () => (
-  <Admin
-    layout={Layout}
-    dataProvider={dataProvider}
-    authProvider={authProvider}
-    loginPage={CustomLogin}
-    theme={myTheme}
-  >
-    <CustomRoutes>
-      <Route path="/selector" element={<Selector />} />
-      <Route path="/urban-form" element={<UrbanForm />} />
-    </CustomRoutes>
+    <Admin
+        layout={Layout}
+        dataProvider={dataProvider}
+        authProvider={authProvider}
+        loginPage={CustomLogin}
+        theme={myTheme}
+    >
+        <CustomRoutes>
+            <Route path="/selector" element={<Selector />} />
+        </CustomRoutes>
 
-    <Resource name="dashboard" list={Dashboard} />
-    <Resource name="estadisticas" list={Estadisticas} />
-    <Resource name="listado" list={Listado} />
-    <Resource name="medicForm" list={ListGuesser} create={MedicFormCreate} show={ShowGuesser} />
-  </Admin>
+        {/* Resource registration */}
+        <Resource name="dashboard" list={Dashboard} icon={DashboardIcon} />
+        <Resource name="estadisticas" list={Estadisticas} icon={BarChartIcon} />
+        <Resource name="listado" list={Listado} icon={ListAltIcon} />
+        <Resource name="medicForm" list={ListGuesser} create={MedicFormCreate} show={ShowGuesser} icon={MedicalInformationIcon} />
+
+        {/* Register the urbanForm resource */}
+        <Resource name="urbanForm" create={UrbanFormCreate} icon={EngineeringIcon} />
+    </Admin>
 );
